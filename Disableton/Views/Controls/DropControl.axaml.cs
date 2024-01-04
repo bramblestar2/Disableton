f86ac7fe99247ee48018346d40ef49ea4aaba939
Jay Feather2000 <thegals4321@gmail.com>
2024-01-04 04:08:17 -0600

    drop control

--- Disableton/Views/Controls/DropControl.axaml.cs
+++ Disableton/Views/Controls/DropControl.axaml.cs
@@ -1,16 +1,25 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
 using Avalonia.Controls.Primitives;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.Threading;
 using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Disableton.Views.Controls
 {
+    [PseudoClasses(pcDragEnter, pcDrop)]
     public class DropControl : TemplatedControl
     {
         public static readonly RoutedEvent<DragEventArgs> FileDropEvent =
             RoutedEvent.Register<DragEventArgs>(nameof(FileDrop), RoutingStrategies.Bubble, typeof(DropControl));
+
+        public const string pcDragEnter = ":drag-enter";
+        public const string pcDrop      = ":drop";
 
         public event EventHandler<DragEventArgs> FileDrop
         {
@@ -20,7 +29,42 @@ namespace Disableton.Views.Controls
 
         public DropControl()
         {
+            this.AddHandler(DragDrop.DropEvent, Drop);
+            this.AddHandler(DragDrop.DragEnterEvent, DragEnter);
+            this.AddHandler(DragDrop.DragLeaveEvent, DragLeave);
+        }
 
+        private void Drop(object? sender, DragEventArgs e)
+        {
+            var files = e.Data.GetFiles();
+            if (files == null || files.Count() == 0) return;
+
+            PseudoClasses.Set(pcDrop, true);
+            PseudoClasses.Set(pcDragEnter, false);
+
+            Task.Delay(1000).ContinueWith(t =>
+            {
+                Dispatcher.UIThread.Invoke(() =>
+                {
+                    PseudoClasses.Set(pcDrop, false);
+                });
+            });
+        }
+
+        private void DragEnter(object? sender, DragEventArgs e)
+        {
+            var files = e.Data.GetFiles();
+            if (files == null || files.Count() == 0) return;
+
+            PseudoClasses.Set(pcDragEnter, true);
+        }
+
+        private void DragLeave(object? sender, DragEventArgs e)
+        {
+            var files = e.Data.GetFiles();
+            if (files == null || files.Count() == 0) return;
+
+            PseudoClasses.Set(pcDragEnter, false);
         }
     }
 }
